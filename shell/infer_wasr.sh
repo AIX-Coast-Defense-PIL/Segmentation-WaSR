1885c8fb926a7a01642a9fa6915033137a776c3a
yoonji811 <yoonji811@gmail.com>
2022-11-22 01:02:32 +0900

    20221122

--- shell/infer_wasr.sh
+++ shell/infer_wasr.sh
@@ -5,6 +5,6 @@ python WaSR/predict.py \
 --dataset_config WaSR/configs/$dataset\_test.yaml \
 --model wasr_resnet101 \
 --weights WaSR/output/logs/wasr_resnet101_pretrained_mastr1478/20221110164130/weights.pt \
---output_dir WaSR/output/predictions/test2 \
+--output_dir WaSR/output/predictions/pretrained_mastr1478/20221110164130_$dataset \
 --mode eval
 # --mode pred
